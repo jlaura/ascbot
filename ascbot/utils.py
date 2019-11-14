0165cee811b0f7284f0d775e776615a1f384040c
Jay <jlaura@asu.edu>
2019-11-14 08:12:48 -0700

    Testing newline strip for keys

--- ascbot/utils.py
+++ ascbot/utils.py
@@ -1,6 +1,6 @@
 def get_secret(secret_name):
     try:
         with open(f'/run/secrets/{secret_name}', 'r') as secret_file:
-            return secret_file.read()
+            return secret_file.read().rstrip()
     except IOError:
         return None
